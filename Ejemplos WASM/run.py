38d0e5bbd630c4b8256d10aa8c1343b48975e160
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2026-04-07 21:49:51 -0600

    Add WebAssembly functions for multiplication and Fahrenheit to Celsius conversion

--- Ejemplos WASM/run.py
+++ Ejemplos WASM/run.py
@@ -14,6 +14,13 @@ def call_wasm_fun(file_name, fn_name, *args):
 def main():
     print(call_wasm_fun('simple.wat', 'meaning_of_life'))
     print(call_wasm_fun('simple.wat', 'multiply', 2, 3))
+    print(call_wasm_fun('simple.wat', 'f', 0))
+    print(call_wasm_fun('simple.wat', 'f', 1))
+    print(call_wasm_fun('simple.wat', 'f', 2))
+    print(call_wasm_fun('simple.wat', 'f', 10))
+    print(call_wasm_fun('simple.wat', 'f2c', 212.0))
+    print(call_wasm_fun('simple.wat', 'f2c', 32.0))
+    print(call_wasm_fun('simple.wat', 'f2c', -40.0))
 
 
 if __name__ == '__main__':
