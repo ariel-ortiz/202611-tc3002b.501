e9a9f6eba535c3220b97db9d5e769da8f9c605fa
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2026-05-26 21:21:51 -0600

    Step 10: While statements

---
+++ project/ex_10_while.py
@@ -0,0 +1,21 @@
+from delta import Compiler, Phase
+
+source = '''
+
+    var n, r, i;
+    n = 5;
+    r = 1;
+    i = 0;
+    while n - i {
+        i = i + 1;
+        r = r * i;
+    }
+    r
+
+'''
+
+c = Compiler('program')
+c.realize(source)
+# print(c.parse_tree_str)
+# print(c.wat_code)
+print(c.result)
